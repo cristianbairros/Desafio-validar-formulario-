113476a5a02bf6f4855aec978c460b6d96be0161
cristian bairros <cristianmil7hor@gmail.com>
2022-10-21 18:59:41 -0300

    atualização
    atualização

---
+++ script.js
@@ -0,0 +1,23 @@
+let campos = document.querySelectorAll('#input');
+let span = document.querySelectorAll('#span');
+let botao = document.getElementById('botao');
+botao.addEventListener('click', validarForm);
+
+function validarForm() {
+    for (let i = 0; i < campos.length; i++) {
+        if (campos[i].value.length >= 6) {
+            campos[i].classList.add("borda-verde")
+        }
+        else if (campos[i].value.length < 6) {
+            campos[i].classList.add("borda-vermelha")
+            span[i].classList.remove("span")
+        }
+    }
+}
+
+
+
+
+
+
+
