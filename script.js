abd459e0f8bb55fb8910a79b1408ac91c9d69d90
cristian bairros <cristianmil7hor@gmail.com>
2022-10-30 19:05:57 -0300

    adição  de spans
    adição  de spans com alerta de validação

--- script.js
+++ script.js
@@ -2,18 +2,17 @@ let campos = document.querySelectorAll('.campo');
 let span = document.querySelectorAll('#span');
 let botao = document.getElementById('botao');
 botao.addEventListener('click', () => {
-
-campos.forEach((item,indice) =>{
-if(campos[indice].value !== '' ){  
-campos[indice].classList.remove("borda-vermelha")
-span[indice].classList.add("span")
-campos[indice].classList.add("borda-verde")
-}
-else{
-campos[indice].classList.add("borda-vermelha")
-span[indice].classList.remove("span")
-}
-});
+    campos.forEach((item, indice) => {
+        if (campos[indice].value !== '') {
+            campos[indice].classList.remove("borda-vermelha")
+            span[indice].classList.add("span")
+            campos[indice].classList.add("borda-verde")
+        }
+        else {
+            campos[indice].classList.add("borda-vermelha")
+            span[indice].classList.remove("span")
+        }
+    });
 });
 
 
